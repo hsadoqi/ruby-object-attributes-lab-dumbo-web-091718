a0ad89aa4264c26fcf8369ca215fce6cb2f0a9ae
Hanaa Sadoqi <hsadoqi@gmail.com>
2018-09-19 15:25:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog 
-  
-  attr_accessor :name
-  
   def initialize(name)
     @name = name
   end 
   
+  def name 
+    return @name
+  end 
+  
+  def name=(new_name)
+    @name = new_name
+  end 
 end 
